0fb089177664f3cfa7fc67afeebc051b8026f129
Rebecca Zeng <rebeccazeng@berkeley.edu>
2018-05-28 16:15:51 -0700

    Added dashboard prototype

---
+++ src/index.js
@@ -0,0 +1,33 @@
+class DashboardOption extends React.Component {
+  render() {
+    return (
+      <a>
+        {this.props.value}
+        </a>
+    );
+  }
+}
+
+class Dashboard extends React.Component {
+  renderDashboardOption(option) {
+    return <DashboardOption value={option} />;
+  }
+  render() {
+    return (
+      <div className="dashboard">
+        <div className="dashboard-option" id="first-menu-item">{this.renderDashboardOption("CollegePrep.org")}</div>
+        <div className="dashboard-option">{this.renderDashboardOption("Find a Tutor")}</div>
+        <div className="dashboard-option">{this.renderDashboardOption("Become a Tutor")}</div>
+        <div className="dashboard-option">{this.renderDashboardOption("Pricing")}</div>
+        <div className="dashboard-option">{this.renderDashboardOption("About")}</div>
+       
+      </div>
+    );
+  }
+}
+//========================================
+
+ReactDOM.render(
+  <Dashboard />,
+  document.getElementById('root')
+);

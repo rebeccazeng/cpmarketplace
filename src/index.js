c0bc38f7c036a9589577617bb3ca633cba52529d
Rebecca Zeng <rebeccazeng@berkeley.edu>
2018-05-28 19:29:34 -0700

    Changed name to navbar and added selection box

--- src/index.js
+++ src/index.js
@@ -1,32 +1,60 @@
-class DashboardOption extends React.Component {
+class NavbarOption extends React.Component {
+  render() {
+    return <a>{this.props.value}</a>;
+  }
+}
+
+class Navbar extends React.Component {
+  renderNavbarOption(option) {
+    return <NavbarOption value={option} />;
+  }
   render() {
     return (
-      <a>
-        {this.props.value}
-        </a>
+      <div className="navbar">
+        <div className="navbar-option" id="bold-item">
+          {this.renderNavbarOption("CollegePrep.org")}
+        </div>
+        <div className="navbar-option">
+          {this.renderNavbarOption("Find a Tutor")}
+        </div>
+        <div className="navbar-option">
+          {this.renderNavbarOption("Become a Tutor")}
+        </div>
+        <div className="navbar-option">
+          {this.renderNavbarOption("Pricing")}
+        </div>
+        <div className="navbar-option">
+          {this.renderNavbarOption("About")}
+        </div>
+
+        <div className="navbar-option" id="navbar-right">
+          {this.renderNavbarOption("Sign Up")}
+        </div>
+        <div className="navbar-option" id="navbar-right">
+          {this.renderNavbarOption("Log In")}
+        </div>
+      </div>
     );
   }
 }
 
-class Dashboard extends React.Component {
-  renderDashboardOption(option) {
-    return <DashboardOption value={option} />;
-  }
+class SelectionBox extends React.Component {
   render() {
     return (
-      <div className="dashboard">
-        <div className="dashboard-option" id="bold-menu-item">{this.renderDashboardOption("CollegePrep.org")}</div>
-        <div className="dashboard-option">{this.renderDashboardOption("Find a Tutor")}</div>
-        <div className="dashboard-option">{this.renderDashboardOption("Become a Tutor")}</div>
-        <div className="dashboard-option">{this.renderDashboardOption("Pricing")}</div>
-        <div className="dashboard-option">{this.renderDashboardOption("About")}</div>
+      <div className="selectionBox">
+       <div> CollegePrep is here to help you with all you need to improve your score. </div>
         
-        <div className="dashboard-option" id="navbar-right">{this.renderDashboardOption("Sign Up")}</div>
-        <div className="dashboard-option" id="navbar-right">{this.renderDashboardOption("Log In")}</div>
+      </div>
+      )
+  }
+}
 
-         
-
-       
+class Page extends React.Component {
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <SelectionBox />
       </div>
     );
   }
@@ -34,6 +62,7 @@ class Dashboard extends React.Component {
 //========================================
 
 ReactDOM.render(
-  <Dashboard />,
-  document.getElementById('root')
+  <Page />,
+  // <SelectionBox />,
+  document.getElementById("root")
 );
